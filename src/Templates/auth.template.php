59bb5980345a023bc331166e12aee47c626f5ed7
Artem Anoshin <artem-anoshin@yandex.ru>
2024-05-10 09:43:16 +0400

    Added styles uikit

--- src/Templates/auth.template.php
+++ src/Templates/auth.template.php
@@ -7,7 +7,6 @@
     <title>Аутентификация</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="/uikit.min.css" rel="stylesheet">
 </head>
 <body style="display: flex;
     align-items: center;
